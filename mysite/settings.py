b9136af2b198afda9d713cf6c15562379fccadb5
YohabPark <dyqkr@naver.com>
2023-01-30 13:55:30 +0900

    Add geeks app

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'geeks.apps.GeeksConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls', include('polls.urls')),
+    # path('geeks', include('geeks.urls')),
+    path('geeks/', include('geeks.urls')),
     path('admin/', admin.site.urls),
 ]
